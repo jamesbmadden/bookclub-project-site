f8f764b84348cc21f4767de5ff0d2456628a28d5
James Madden <jamesbmadden@gmail.com>
2019-04-28 18:45:54 -0700

    Add Styles to Quote

--- src/blockquote/blockquote.js
+++ src/blockquote/blockquote.js
@@ -7,18 +7,34 @@ export default class Blockquote extends LitElement {
 
   static get styles () {
     return css`
-
+      .container {
+        position: relative;
+        display: inline-block;
+        width: 50%;
+        color: white;
+      }
+      .decoration {
+        color: #555;
+        position: absolute;
+        left: 8px;
+        font-weight: 900;
+        font-size: 8rem;
+        z-index: -1;
+      }
+      blockquote {
+        padding-top: 1rem;
+      }
     `;
   }
 
   render () {
     return html`
-      <span class="decoration">“</span>
       <div class="container">
+        <span class="decoration">“</span>
         <blockquote>
           <slot></slot>
         </blockquote>
-        <cite>${this.credit}</cite>
+        -<cite>${this.credit}</cite>
       </div>
     `;
   }
