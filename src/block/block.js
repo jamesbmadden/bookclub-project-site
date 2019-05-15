9ae7aca7c335b96b094b4ac8806f9d923581c939
James Madden <jamesbmadden@gmail.com>
2019-05-15 11:24:03 -0700

    Add BlockContainer

--- src/block/block.js
+++ src/block/block.js
@@ -2,6 +2,52 @@ import { LitElement, html, customElement, property, css } from 'https://unpkg.co
 
 @customElement('bc-block')
 export default class Block extends LitElement {
+
   @property({ type: Number })
-  width = 50;
+  fraction = 2;
+
+  static get styles () {
+    return css`
+      :host {
+        display: inline-block;
+        position: relative;
+      }
+    `;
+  }
+
+  render () {
+    return html`
+      <style>
+        :host {
+          width: calc(100% / ${this.fraction});
+        }
+      </style>
+      <slot></slot>
+    `;
+  }
+
+}
+
+@customElement('bc-block-container')
+export class BlockContainer extends LitElement {
+
+  static get styles () {
+    return css`
+      .container {
+        display: flex;
+        position: relative;
+        width: 100%;
+        flex-direction: row;
+      }
+    `;
+  }
+
+  render () {
+    return html`
+      <div class="container">
+        <slot></slot>
+      </div>
+    `;
+  }
+
 }
