119be00c42154bff384f5cc744869a6c30689021
James Madden <jamesbmadden@gmail.com>
2019-05-25 13:51:03 -0700

    Add BC-Event element

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './header/header.js';
 import './blockquote/blockquote.js';
 import './block/block.js';
 import './character/character.js';
+import './event/event.js';
 import 'https://unpkg.com/@graviton/button@1.1.9/index.js?module';
 
 console.log('hello world!');
