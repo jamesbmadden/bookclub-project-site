4f0ade9fa685b735a8665042a8378b7c52afc3c2
James Madden <jamesbmadden@gmail.com>
2019-05-30 17:41:51 -0700

    Remove Anchor Elements

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './blockquote/blockquote.js';
 import './block/block.js';
 import './character/character.js';
 import './event/event.js';
-import './anchor/anchor.js';
 import 'https://unpkg.com/@graviton/button@1.1.9/index.js?module';
 
 console.log('hello world!');
